0e1306aa0820aebfaf57a56fbdbd8441bf956895
Sergii Stoiachko <119351759+SergiiSt@users.noreply.github.com>
2025-08-22 18:21:36 +0200

    counter

---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+
+  return (
+    <>
+      <h1>{count}</h1>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+    </>
+  );
+}
